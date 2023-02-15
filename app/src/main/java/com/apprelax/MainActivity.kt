a91d1e9ef24a8ffaf5d8b5051dcdf294bd5913a3
luis <luis.hev1920@gmail.com>
2023-02-15 15:34:49 -0600

    BlototthPrueba

---
+++ app/src/main/java/com/apprelax/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.apprelax
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.ingenieriajhr.blujhr.BluJhr
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var blue: BluJhr
+    var devicesBluetooth = ArrayList<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        blue = BluJhr(this)
+        blue.onBluetooth()
+
+        listDeviceBluetooth.setOnItemClickListener { adapterView, view, i, l ->
+            if (devicesBluetooth.isNotEmpty()){
+                blue.connect(devicesBluetooth[i])
+                blue.setDataLoadFinishedListener(object:BluJhr.ConnectedBluetooth{
+                    override fun onConnectState(state: BluJhr.Connected) {
+                        when(state){
+
+                            BluJhr.Connected.True->{
+                                Toast.makeText(applicationContext,"True",Toast.LENGTH_SHORT).show()
+                                listDeviceBluetooth.visibility = View.GONE
+                                viewConn.visibility = View.VISIBLE
+                                rxReceived()
+                            }
+
+                            BluJhr.Connected.Pending->{
+                                Toast.makeText(applicationContext,"Pending",Toast.LENGTH_SHORT).show()
+
+                            }
+
+                            BluJhr.Connected.False->{
+                                Toast.makeText(applicationContext,"False",Toast.LENGTH_SHORT).show()
+                            }
+
+                            BluJhr.Connected.Disconnect->{
+                                Toast.makeText(applicationContext,"Disconnect",Toast.LENGTH_SHORT).show()
+                                listDeviceBluetooth.visibility = View.VISIBLE
+                                viewConn.visibility = View.GONE
+                            }
+
+                        }
+                    }
+                })
+            }
+        }
+        //Para enviar datos
+        buttonSend.setOnClickListener {
+            blue.bluTx(edtTx.text.toString())
+        }
+        //Para desconectarse
+        buttonSend.setOnLongClickListener {
+            blue.closeConnection()
+            true
+        }
+
+
+    }
+
+    //Funcion que recibe datos
+    private fun rxReceived() {
+        blue.loadDateRx(object:BluJhr.ReceivedData{
+            override fun rxDate(rx: String) {
+                consola.text = consola.text.toString()+rx
+            }
+        })
+    }
+
+    /**
+     * pedimos los permisos correspondientes, para android 12 hay que pedir los siguientes BLUETOOTH_SCAN y BLUETOOTH_CONNECT
+     * en android 12 o superior se requieren permisos adicionales
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        if (blue.checkPermissions(requestCode,grantResults)){
+
+            Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show()
+            blue.initializeBluetooth()
+
+        }else{
+            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.S){
+                blue.initializeBluetooth()
+            }else{
+                Toast.makeText(this, "Algo salio mal", Toast.LENGTH_SHORT).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (!blue.stateBluetoooth() && requestCode == 100){
+            blue.initializeBluetooth()
+        }else{
+            if (requestCode == 100){
+                devicesBluetooth = blue.deviceBluetooth()
+                if (devicesBluetooth.isNotEmpty()){
+                    val adapter = ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,devicesBluetooth)
+                    listDeviceBluetooth.adapter = adapter
+                }else{
+                    Toast.makeText(this, "No tienes vinculados dispositivos", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
